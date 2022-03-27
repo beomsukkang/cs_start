6eb3edc0c5c5014dc58903669572b72ebdfaa084
bumsug02@naver.com <bumsug02@naver.com>
2022-03-27 15:25:48 +0700

    조건 연산시간설정

---
+++ ConditionalOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ConditionalOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string result = (10 % 3) == 0 ? "짝수" : "홀수";
+
+            Console.WriteLine(result);
+        }
+    }
+}
