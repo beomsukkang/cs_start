f9c0436c4f6ace2529d41bc508d6415578d28982
bumsug02@naver.com <bumsug02@naver.com>
2022-04-06 07:18:38 +0700

    클래스

---
+++ BasicClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} :야옹");
+        }
+    }
+    
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            cat kitty = new cat(); //생성자
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} :{kitty.Color}");
+
+            cat nero = new cat();
+            nero.Color = "검은색";
+            nero.Name = "네로";
+
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} :{nero.Color}");
+        }
+    }
+}
