a7dd6119df82bc86999faaad6df86cbfdbe28264
bumsug02@naver.com <bumsug02@naver.com>
2022-04-11 07:14:32 +0700

    접근 한정자(protected)

---
+++ AccessModifier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    class WaterHeater
+    {
+        protected int temperature;
+
+        public void SetTemperature(int temperature)
+        {
+            if(temperature < -5 || temperature > 42)
+            {
+                throw new Exception("Out of temperature range");
+            }
+
+            this.temperature = temperature;
+
+        }
+        
+        internal void TurnOnWater()
+        {
+            Console.WriteLine($"Turn on water : {temperature}");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WaterHeater heater = new WaterHeater();
+                heater.SetTemperature(20);
+                heater.TurnOnWater();
+
+                heater.SetTemperature(-2);
+                heater.TurnOnWater();
+
+                heater.SetTemperature(50);
+                heater.TurnOnWater();
+            }
+            catch (Exception e)         //try~catch는 예외처리 위해 사용.
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
