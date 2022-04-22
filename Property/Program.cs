2800229bbf3180697db44dda8f5050b0b26df463
bumsug02@naver.com <bumsug02@naver.com>
2022-04-22 21:40:26 +0700

    프로퍼티

---
+++ Property/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Property
+{
+    class BirthdayInfo
+    {
+        private string name;
+        private DateTime birthday;
+
+        public string Name
+        {
+            get { return name; } //get 접근자, 구현 안할 시 쓰기전용
+            set { name = value; } //set 접근자, 구현 안할 시 읽기전용
+        }
+
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            birth.Name = "서현";
+            birth.Birthday = new DateTime(1991, 6, 28);
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+}
