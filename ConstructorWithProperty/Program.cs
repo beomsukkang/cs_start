a08e394552af96d3f0448a48efe3ac78fe92acb4
bumsug02@naver.com <bumsug02@naver.com>
2022-04-23 00:04:43 +0700

    프로퍼티와 생성자

---
+++ ConstructorWithProperty/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//객체를 생성할 때 프로퍼티를 이용한 각 필드 초기화
+
+namespace ConstructorWithProperty
+{
+    class BirthdayInfo
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo()
+            {
+                Name = "서현",
+                Birthday = new DateTime(1991, 6, 28)
+            };
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age:{birth.Age}");
+        }
+    }
+}
