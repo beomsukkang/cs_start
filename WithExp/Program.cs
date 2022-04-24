70087599560d35c394ae1cc6efeb913bf3218fc3
bumsug02@naver.com <bumsug02@naver.com>
2022-04-24 12:42:21 +0700

    with을 이용한 레코드 복사

---
+++ WithExp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+/*
+ * C#컴파일러는 레코드 형식을 위한 복사 생성자를 자동으로 작성한다.
+ * 하지만 protected로 선언되기에 with식을 이용해 호출할 수 있다.
+ */
+namespace WithExp
+{
+    record RTransaction
+    {
+        public string From {  get; init; }
+        public string To { get; init; }
+        public int Amount { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10} : ${Amount}";
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            RTransaction tr1 = new RTransaction { From = "Alice", To = "Bob", Amount = 100 }; //alice to bob
+            RTransaction tr2 = tr1 with { To = "Charlie" }; //alice to charlie
+            RTransaction tr3 = tr2 with { From = "Dave", Amount = 30 }; //dave to charlie
+
+            Console.WriteLine(tr1);
+            Console.WriteLine(tr2);
+            Console.WriteLine(tr3);
+        }
+    }
+}
