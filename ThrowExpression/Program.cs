4d72a9ddb34212c06a02e41ab436b987b633efcb
bumsug02@naver.com <bumsug02@naver.com>
2022-05-11 06:35:32 +0700

    예외던지기-Throw를 식으로 사용하기

---
+++ ThrowExpression/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ThrowExpression
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int? a = null;
+                int b = a ?? throw new ArgumentNullException(); //a는 null이므로 b에 a를 할당하지 않고 throw 식 실행
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                int[] array = new[] { 1, 2, 3 };
+                int index = 4;
+                int value = array[
+                    index >= 0 && index < 3
+                    ? index : throw new IndexOutOfRangeException()
+                    ];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
