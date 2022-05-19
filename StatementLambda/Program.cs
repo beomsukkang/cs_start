4488bfacda5f07db7a375855a4c1c8004353d5c1
bumsug02@naver.com <bumsug02@naver.com>
2022-05-19 21:12:58 +0700

    문 형식의 람다식(문제있음)

---
+++ StatementLambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//문장을 입력하면 스페이스를 모두 지우는 프로그램
+namespace StatementLambda
+{
+    class MainApp
+    {
+        delegate string Concatenate(string[] args);
+
+        static void Main(string[] args)
+        {
+            Concatenate concat =
+                ( arr ) =>
+                {
+                    string result = "";
+                    foreach (string s in arr)
+                        result += s;
+
+                    return result;
+                };
+            Console.WriteLine( concat(args) );
+        }
+    }
+}
