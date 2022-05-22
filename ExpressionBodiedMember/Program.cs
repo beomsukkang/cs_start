1d9350bad3f7ea45b656cd13f4e15754a141cf8e
bumsug02@naver.com <bumsug02@naver.com>
2022-05-23 00:19:22 +0700

    식으로 이루어지는 멤버

---
+++ ExpressionBodiedMember/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpressionBodiedMember
+{
+    class FriendList
+    {
+        private List<string> list = new List<string>();
+
+        public void Add(string name) => list.Add(name);
+        public void Remove(string name) => list.Remove(name);
+        public void PrintAll()
+        {
+            foreach (var s in list)
+                Console.WriteLine(s);
+        }
+
+        public FriendList() => Console.WriteLine("Friend List()"); //생성자
+        ~FriendList() => Console.WriteLine("~Friend List()"); //종료자, 언제종료되는지 알수 없어 출력x
+
+        // public int Capacity => list.Capacity; //읽기 전용
+
+        public int Capacity //속성, 읽고쓰기 다됨
+        {
+            get => list.Capacity;
+            set => list.Capacity = value;
+        }
+
+        // public string this[int index] => list.[index]; //읽기 전용
+        public string this[int index] //인덱서, 읽고쓰기 다됨
+        {
+            get => list[index];
+            set => list[index] = value;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            FriendList obj = new FriendList();
+            obj.Add("Eney");
+            obj.Add("Meeny");
+            obj.Add("Miny");
+            obj.Remove("Eney");
+            obj.PrintAll();
+
+            Console.WriteLine($"{obj.Capacity}");
+            obj.Capacity = 10;
+            Console.WriteLine($"{obj.Capacity}");
+
+            Console.WriteLine($"{obj[0]}");
+            obj[0] = "Moe";
+            obj.PrintAll();
+        }
+    }
+}
