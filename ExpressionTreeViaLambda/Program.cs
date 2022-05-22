99173fd3ae381bc7496cb82a2d1b56fd9952c0ff
bumsug02@naver.com <bumsug02@naver.com>
2022-05-23 00:03:19 +0700

    람다식 이용해 더 간편한 식트리 만들기

---
+++ ExpressionTreeViaLambda/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+//UsingExpressionTree와 동일한 출력, Lambda식을 이용해 더 간편하게 만들기.(Expression형식은 불변이기에 인스턴스 한번 만들어지면 변경불가)
+namespace ExpressionTreeViaLambda
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int, int>> expression = 
+                (a, b) => 1*2+(a-b);
+            Func<int, int, int> func = expression.Compile();
+
+            //x=7, y=8
+            Console.WriteLine($"1*2+({7}-{8}) = {func(7, 8)}");
+
+        }
+    }
+}
