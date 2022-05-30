98928277a961f8276d5ad801f1a04f292588c081
bumsug02@naver.com <bumsug02@naver.com>
2022-05-30 23:50:57 +0700

    동적 인스턴스 생성, 프로퍼티에 동적으로 값 할당 후 메소드 호출하는 프로그램

---
+++ DynamicInstance/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynamicInstance
+{
+    class Profile
+    {
+        private string name;
+        private string phone;
+        public Profile()
+        {
+            name = "";
+            phone = "";
+        }
+
+        public Profile(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{name}, {phone}");
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("DynamicInstance.Profile");
+            MethodInfo methodInfo = type.GetMethod("Print");
+
+            PropertyInfo nameProperty = type.GetProperty("Name");
+            PropertyInfo phoneProperty = type.GetProperty("Phone");
+
+            object profile = Activator.CreateInstance(type, "박상현", "512-1234");
+            methodInfo.Invoke(profile, null); //null 인수가 오는 자리에는 Invoke() 메소드가 호출할 메소드의 인수가 와야한다. 여기선 메소드의 인수가 없기에 null을 넘긴다.
+
+            profile = Activator.CreateInstance(type);
+            nameProperty.SetValue(profile, "박찬호", null);
+            phoneProperty.SetValue(profile, "997-5511", null);
+
+            Console.WriteLine("{0}, {1}",
+                nameProperty.GetValue(profile, null),
+                phoneProperty.GetValue(profile, null));
+        }
+    }
+}
