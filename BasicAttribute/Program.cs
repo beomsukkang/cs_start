b942de36d285197fd24150bf8959f4c1f9f25dee
bumsug02@naver.com <bumsug02@naver.com>
2022-05-31 19:24:31 +0700

    Attribute

---
+++ BasicAttribute/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//에트리뷰트란 코드에 대한 부가 정보를 기록하고 읽을 수 있는 기능.
+namespace BasicAttribute
+{
+    class MyClass
+    {
+        [Obsolete("OldMethod는 폐기되었습니다. NewMethod()를 이용하세요.")]
+        public void OldMethod()
+        {
+            Console.WriteLine("I'm old");
+        }
+
+        public void NewMethod()
+        {
+            Console.WriteLine("I'm new");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+
+            obj.OldMethod();
+            obj.NewMethod();
+
+        }
+    }
+}
