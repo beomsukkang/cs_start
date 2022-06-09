066b7e789ec45fe3efcc69a60f97af4bca1038b7
bumsug02@naver.com <bumsug02@naver.com>
2022-06-09 21:38:43 +0700

    디렉터리/ 파일 정보 조회하기

---
+++ Dir/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Dir
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            if (args.Length < 1)
+                directory = ".";
+            else
+                directory = args[0];
+
+            Console.WriteLine($"{directory} directory Info");
+            Console.WriteLine("- Directories :");
+            var directories = (from dir in Directory.GetDirectories(directory) //하위 디렉터리 목록 조회
+                               let info = new DirectoryInfo(dir)
+                               select new
+                               {
+                                   Name = info.Name,
+                                   Attributes = info.Attributes
+                               }).ToList();
+
+            foreach (var d in directories)
+                Console.WriteLine($"{d.Name} : {d.Attributes}");
+
+            Console.WriteLine(" - Files : ");
+            var files = (from file in Directory.GetFiles(directory) //하위 파일 목록 조회
+                         let info = new FileInfo(file) //let은 LINQ 안에서 변수를 만든다. LINQ의 var라고 생각하면 이해하기 편하다.
+                         select new
+                         {
+                             Name = info.Name,
+                             FileSize = info.Length,
+                             Attributes = info.Attributes
+                         }).ToList();
+
+            foreach (var f in files)
+                Console.WriteLine(
+                    $"{f.Name} : {f.FileSize}, {f.Attributes}");
+        }
+    }
+}
