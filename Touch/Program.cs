aea330f08dd410d0164e95fc3be27795d00368a8
bumsug02@naver.com <bumsug02@naver.com>
2022-06-12 23:12:22 +0700

    디렉터리/파일 생성하기

---
+++ Touch/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Touch
+{
+    class MainApp
+    {
+        static void OnWrongPathType(string type)
+        {
+            Console.WriteLine($"{type} is wrong type");
+            return;
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine(
+                    "Usage : Touch.exe <Path> [Type:File/Directory]");
+                return;
+            }
+
+            string path  = args[0];
+            string type = "File";
+            if (args.Length > 1)
+                type = args[1];
+
+            if (File.Exists(path) || Directory.Exists(path))
+            {
+                if (type == "File")
+                    File.SetLastWriteTime(path, DateTime.Now);
+                else if (type == "Directory")
+                    Directory.SetLastWriteTime(path, DateTime.Now);
+                else
+                {
+                    OnWrongPathType(path);
+                    return;
+                }
+                Console.WriteLine($"Updated {path} {type}");
+            }
+
+            else
+            {
+                if (type == "File")
+                    File.Create(path).Close();
+                else if (type == "Directory")
+                    Directory.CreateDirectory(path);
+                else
+                {
+                    OnWrongPathType(path);
+                    return;
+                }
+                Console.WriteLine($"Created {path} {type}");
+            }
+        }
+    }
+}
+
+/*
+ cmd 실행 후 C:\Users\bumsu\Desktop\workspace\Touch\bin\Debug\net6.0 경로로 들어간 후, Touch a.dat, Touch MyFolder Directory 를 사용해 파일 및 디렉터리 정보를 생성한다.
+dir로 디렉터리의 정보를 조회할 수 있다.
+Touch.exe a.dat 을 하면 파일이 갱신되어 날짜만 변경된다.
+ */
