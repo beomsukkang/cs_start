06b2e3cf190029ebcec40eaff0b09eb8013bd61e
bumsug02@naver.com <bumsug02@naver.com>
2022-06-12 23:36:32 +0700

    파일을 읽고 쓰기 위해 알아야 할 것들 - WriteByte()메소드와 Seek() 메소드

---
+++ SeqNRand/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace SeqNRand
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Stream outStream = new FileStream("a.dat", FileMode.Create);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x01); // WriteByte() method를 이용해 순차적으로 파일 내의 위치를 옮겨가며 데이터를 기록
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x02);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x03);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Seek(5, SeekOrigin.Current); // Seek() 메소드를 활용해 임의의 주소로 단번에 점프하는 임의 접근 방식 
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x01);
+            Console.WriteLine($"Position : {outStream.Position}");
+        }
+    }
+}
