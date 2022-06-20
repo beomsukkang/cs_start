7bf25a40fe57894de385105673381dc241d5552e
bumsug02@naver.com <bumsug02@naver.com>
2022-06-21 06:30:45 +0700

    스레드간의 동기화

---
+++ Synchronize/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+namespace Synchronize
+{
+    class Counter
+    {
+        const int LOOP_COUNT = 1000;
+
+        readonly object thisLock;
+
+        private int count;
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public Counter()
+        {
+            thisLock = new object();
+            count = 0;
+        }
+
+        public void Increase()
+        {
+            int loopCount = LOOP_COUNT;
+
+            while (loopCount-- > 0)
+            {
+                lock (thisLock) //lock 키워드와 중괄호로 둘러쌓인 이 부분은 크리티컬 섹션이 된다. 한 스레드가 이 코드를 실행하다가 lock 블록이 끝나는 괄호를 만나기 전까지 다른 스레드는 절대 이 코드를 실행할 수 없다.
+                {
+                    count++;
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        public void Decrease()
+        {
+            int loopCount = LOOP_COUNT;
+
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    count--;
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Thread incThread = new Thread(
+                new ThreadStart(counter.Increase));
+            Thread decThread = new Thread(
+                new ThreadStart(counter.Decrease));
+
+            incThread.Start();
+            decThread.Start();
+
+            incThread.Join();
+            decThread.Join();
+
+            Console.WriteLine(counter.Count);
+        }
+    }
+}
