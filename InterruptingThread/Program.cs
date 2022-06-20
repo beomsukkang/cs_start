7727bd6d8a7e89293db8c9b13cd40db99f564d0a
bumsug02@naver.com <bumsug02@naver.com>
2022-06-21 06:21:19 +0700

    인터럽트를 이용한 스레드 임의종료

---
+++ InterruptingThread/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Threading;
+
+namespace InterruptingThread
+{
+    class SideTask
+    {
+        int count;
+
+        public SideTask(int count)
+        {
+            this.count = count;
+        }
+
+        public void KeepAlive()
+        {
+            try
+            {
+                Console.WriteLine("Running thread isn't gonna be interrupted");
+                Thread.SpinWait(1000000000); //SpinWait() 메소드는 Sleep() 메소드와는 달리 스레드가 Running 상태를 유지하게 한다.
+                                             //이 메소드를 사용한 건 Interrupt() 메소드를 호출하는 시점에 스레드가 Running 상태를 유지하도록 하기 위해서이다.
+                                             //Interrupt() 메소드가 호출된 스레드는 Thread.sleep(10)에 의해 WaitJoinSleep 상태로 들어가고 이때 인터럽트가 발생한다.
+
+                while (count > 0)
+                {
+                    Console.WriteLine($"{count--} left");
+
+                    Console.WriteLine("Entering into WaitJoinSleep State...");
+                    Thread.Sleep(10);
+                }
+                Console.WriteLine("Count : 0");
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Clearing resource...");
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            SideTask task = new SideTask(100);
+            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            t1.IsBackground = false;
+
+            Console.WriteLine("Starting thread...");
+            t1.Start();
+
+            Thread.Sleep(100);
+
+            Console.WriteLine("Interrupting thread...");
+            t1.Interrupt(); //스레드 취소(종료)
+
+            Console.WriteLine("Waiting until thread stops...");
+            t1.Join();
+
+            Console.WriteLine("Finished");
+
+
+        }
+    }
+}
