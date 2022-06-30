22bab11e6fdc12d9593289a33a36a6a6084f1a4c
bumsug02@naver.com <bumsug02@naver.com>
2022-06-30 20:30:18 +0700

    응용프로그램을 종료하는 메소드

---
+++ UsingApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UsingApplication
+{
+    class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            MainApp form = new MainApp();
+
+            form.Click += new EventHandler( //Click 이벤트는 윈도우를 클릭했을 때 발생하는 이벤트이다. 윈도우를 클릭하면 Application.Exit()를 호출한다. 
+                (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Closing Window...");
+                    Application.Exit();
+                });
+
+            Console.WriteLine("Starting window Application...");
+            Application.Run(form);
+
+            Console.WriteLine("Exiting Window Application...");
+        }
+    }
+}
