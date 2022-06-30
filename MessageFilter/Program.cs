d11c4c3f2aeceb04c87b220e48ad68b2f29297cc
bumsug02@naver.com <bumsug02@naver.com>
2022-07-01 06:56:04 +0700

    메세지필터-마우스 관련 행동 검출

---
+++ MessageFilter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MessageFilter
+{
+    class MessageFilter : IMessageFilter
+    {
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x0F || m.Msg == 0xA0 ||
+                m.Msg == 0x200 || m.Msg == 0x113) //마우스 움직임 걸러냄
+                return false;
+
+            Console.WriteLine($"{m.ToString()} : {m.Msg}");
+
+            if ( m.Msg == 0x201)
+                Application.Exit();
+
+            return true;
+        }
+    }
+
+    class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            Application.AddMessageFilter(new MessageFilter());
+            Application.Run(new MainApp());
+        }
+    }
+}
