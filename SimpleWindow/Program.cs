170e48e8d30eb65632c8e50fd02957c49b4737ea
bumsug02@naver.com <bumsug02@naver.com>
2022-06-30 20:18:24 +0700

    윈폼 시작

---
+++ SimpleWindow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+/* SimpleWindow.csproj에서
+ <Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>net6.0-Windows</TargetFramework>
+	<UseWindowsForms>true</UseWindowsForms>
+    <DisableWinExeOutputInference>true</DisableWinExeOutputInference>
+  </PropertyGroup>
+
+</Project>
+
+ */
+namespace SimpleWindow
+{
+    class MainApp : System.Windows.Forms.Form
+    {
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Application.Run(new MainApp()); //Application.Run() 메소드에 MainApp의 인스턴스를 인수로 넘겨 호출한다.
+        }
+    }
+}
