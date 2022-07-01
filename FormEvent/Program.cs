7c09e1353e7241a20da143d10337a1c8d4d933b2
bumsug02@naver.com <bumsug02@naver.com>
2022-07-02 06:33:04 +0700

    폼 위에서 마우스를 누를때마다 이벤트를 발생시킨 객체, 마우스버튼, 마우스 커서의 좌표 등을 콘솔에 출력하기

---
+++ FormEvent/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormEvent
+{
+    class MainApp : Form
+    {
+        public void MyMouseHandler(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine($"Sender : {((Form)sender).Text}");
+            Console.WriteLine($"X:{e.X}:{e.Y}"); //마우스 이벤트가 발생한 좌표
+            Console.WriteLine($"Button:{e.Button}, Clicks:{e.Clicks}"); //어떤버튼에서 이벤트가 발생했는지, 몇번 클릭했는지
+            Console.WriteLine();
+        }
+
+        public MainApp(string title)
+        {
+            this.Text = title;
+            this.MouseDown +=
+                new MouseEventHandler(MyMouseHandler);
+        }
+
+        static void Main(string[] args)
+        {
+            Application.Run(new MainApp("Mouse Event Test"));
+        }
+    }
+}
