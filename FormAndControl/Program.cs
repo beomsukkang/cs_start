8280778c8907651f87611621156f8b6c9f6181b3
bumsug02@naver.com <bumsug02@naver.com>
2022-07-05 22:21:32 +0700

    윈폼-Form위에 컨트롤 올리기

---
+++ FormAndControl/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormAndControl
+{
+    class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button(); //컨트롤의 인스턴스 생성
+
+            button.Text = "Click me!"; //컨트롤의 프로퍼티에 값 지정
+            button.Left = 100;
+            button.Top = 50;
+
+            button.Click +=             //컨트롤의 이벤트에 이벤트 처리기 등록
+                (object sender, EventArgs e) =>
+                {
+                    MessageBox.Show("딸깍!");
+                };
+
+            MainApp form = new MainApp();   //폼에 컨트롤 추가
+            form.Text = "Form & Control";
+            form.Height = 150;
+
+            form.Controls.Add(button);
+
+            Application.Run(form);
+        }
+    }
+}
